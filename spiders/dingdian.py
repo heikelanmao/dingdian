7d14f357b52a5353ead929540949f6125df86683
thsheep <thsheep@live.com>
2016-12-25 23:27:15 +0800

    更改了致命错误（多次循环）

--- spiders/dingdian.py
+++ spiders/dingdian.py
@@ -18,14 +18,14 @@ class Myspider(scrapy.Spider):
         for i in range(1, 11):
             url = self.bash_url + str(i) + '_1' + self.bashurl
             yield Request(url, self.parse)
-        yield Request('http://www.23wx.com/quanben/1', self.parse)
+        #yield Request('http://www.23wx.com/quanben/1', self.parse)
 
     def parse(self, response):
         max_num = BeautifulSoup(response.text, 'lxml').find('div', class_='pagelink').find_all('a')[-1].get_text()
-        for i in range(1, 11):
-            for num in range(1, int(max_num) + 1):
-                url = self.bash_url + str(i) + '_' + str(num) + self.bashurl
-                yield Request(url, callback=self.get_name)
+        bashurl = str(response.url)[:-7]
+        for num in range(1, int(max_num) + 1):
+            url = bashurl + '_' + str(num) + self.bashurl
+            yield Request(url, callback=self.get_name)
                 """yieid Request，请求新的URL，后面跟的是回调函数，你需要哪一个函数来处理这个返回值，就调用那一个函数，
                     返回值会以参数的形式传递给你所调用的函数。
                 """
